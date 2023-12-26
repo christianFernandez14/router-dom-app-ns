629ae7ea4b660f47081578eec7386c35f42780f1
Christian Fernandez <christian14@gmail.com>
2023-12-26 16:17:35 -0300

    Estructura basica para trabajar / Switch / Route / Link (137)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-
+import { Switch, Route, Link } from "react-router-dom";
 
 function App() {
 
   return (
     <div>
-      React - roter - Dom
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Inicio</Link>
+          </li>
+          <li>
+            <Link to='/portafolio'>Portafolio</Link>
+          </li>
+        </ul>
+      </nav>
+      <section>
+
+        <Switch>
+
+          <Route exact path='/'>
+            <h1>Inicio</h1>
+          </Route>
+          <Route path='/portafolio'>
+            <h1>Portafolio</h1>
+          </Route>
+
+        </Switch>
+
+      </section>
     </div>
   )
 }
